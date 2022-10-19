d3b249db9c5db3ee9d2ce8bd504cea05b2fc96ac
Kat Chudy <kec21@fsu.edu>
2022-10-19 10:27:35 -0400

    triangle things

--- sketch.js
+++ sketch.js
@@ -4,6 +4,13 @@ function setup() {
 
 function draw() {
   background(200);
-
-
+  quad(1500, 700, 100, 50, 25, 12, 30, 1000)
+  fill("red")
+  stroke("black")
+  strokeWeight(20)
+  quad(400, 100, 500, 240, 560, 420, 160, 180)
+  fill("blue")
+  strokeWeight(10)
+  quad(200, 700, 450, 120, 250, 200, 100, 100)
+  
 }
